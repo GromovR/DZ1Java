ed4dc04741e86d53e1ad7f84733dc773972787e1
Gromov Roman <romzes.rz@gmail.com>
2023-08-05 20:10:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int money = 13676; // количество потраченых денег
+        int bonus = money / 20; //количество начисляемых бонусов
+        System.out.println(bonus);
+
+    }
+}
